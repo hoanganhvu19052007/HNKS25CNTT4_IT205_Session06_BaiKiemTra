ab4308e0f70d6a0d480dbd4a0dd7af48a73023c4
hoanganhvu19052007 <hoangvu2007hg@gmail.com>
2026-05-22 10:00:27 +0700

    Update bai3.py

--- baikiemtra/bai3.py
+++ baikiemtra/bai3.py
@@ -4,16 +4,15 @@ quantity_count = 0
 while True :    
     total_quantity = int(input("Nhập vào tổng số lượng sản phẩm: "))
 
-    total_box = int(input("Nhập vào số thùng hàng: "))
-    if total_box < 0 :
+    if total_quantity < 0 :
         print("Số lượng không hợp lệ, bỏ qua thùng này!")
         continue
-    elif total_box == 0 :
+    elif total_quantity == 0 :
         print("Đã kiểm đếm xong")
         break
     else :
         quantity_count += total_quantity
-        box_count += total_box
+        box_count += 1
 
 print("Tổng số thùng hàng hợp lệ: ", box_count)
 print("Tổng số lượng sản phẩm thu được: ", quantity_count)
